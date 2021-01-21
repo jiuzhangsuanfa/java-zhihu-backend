0499d57451e1f35a912ba9f9a913a87a6b698b3e
Devin Don <i.inf@outlook.com>
2021-01-21 11:23:57 +0800

    chore(script): fix cmd shell

--- script/start-keycloak.cmd
+++ script/start-keycloak.cmd
@@ -1,7 +1,2 @@
 @echo off
-docker run -d \
-  --name zhihu-keycloak \
-  -p 8080:8080 \
-  -e KEYCLOAK_USER=jiuzhang \
-  -e KEYCLOAK_PASSWORD=jiuzhang \
-  quay.io/keycloak/keycloak:12.0.1
+docker run -d --name zhihu-keycloak -p 8080:8080 -e KEYCLOAK_USER=jiuzhang -e KEYCLOAK_PASSWORD=jiuzhang quay.io/keycloak/keycloak:12.0.1
--- script/start-redis.cmd
+++ script/start-redis.cmd
@@ -1,6 +1,2 @@
 @echo off
-docker run -d \
-  --name zhihu-redis \
-  -p 6379:6379 \
-  -v zhihu-redis:/data \
-  redis:6.0.9
+docker run -d --name zhihu-redis -p 6379:6379 -v zhihu-redis:/data redis:6.0.9
