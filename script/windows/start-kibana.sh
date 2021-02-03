160fe55c688165ea146996418eaa3b5e6f17c794
Devin Don <i.inf@outlook.com>
2021-02-03 16:45:13 +0800

    chore(script/windows): fix kibana

--- script/windows/start-kibana.sh
+++ script/windows/start-kibana.sh
@@ -1,2 +1,2 @@
 @echo off
-docker run -d --name zhihu-kibana -p 5601:5601 -e ELASTICSEARCH_HOSTS=http://zhihu-elasticsearch:9200 --network zhihu kibana:6.0.9
+docker run -d --name zhihu-kibana -p 5601:5601 -e ELASTICSEARCH_HOSTS=http://zhihu-elasticsearch:9200 --network zhihu kibana:7.10.1
